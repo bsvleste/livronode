e646dbd17d9d92d3fc11b8bb83dac1a6d979c662
Bruno pc CCSP <brunoccsp@gmail.com>
2018-08-23 16:48:44 -0300

    Crud completo

---
+++ btalk/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const  consign = require('consign');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const methodOverride = require('method-override');
+const error = require('./middlewares/error');
+const app = express();
+
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine','ejs');
+app.use(cookieParser('btalk'));
+app.use(expressSession({
+  name:'btalk.sid',
+  secret:'btalk'
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname,'public')));
+
+consign({})
+.include('models')
+.then('controllers')
+.then('routes')
+.into(app);
+
+app.use(error.notFound);
+app.use(error.serverError);
+
+app.listen(3000,()=>{
+  console.log('Btalk In the House');
+})
