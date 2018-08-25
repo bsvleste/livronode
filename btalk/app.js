c88b4008d0990415cbf2dedf6771c202c7e7ac1e
Bruno pc CCSP <brunoccsp@gmail.com>
2018-08-25 12:58:37 -0300

    chat funcionando

--- btalk/app.js
+++ btalk/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
-const  consign = require('consign');
+const http = require('http');
+const socketIO = require('socket.io'); 
+const consign = require('consign');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const expressSession = require('express-session');
 const methodOverride = require('method-override');
 const error = require('./middlewares/error');
 const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
 
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine','ejs');
@@ -26,9 +30,15 @@ consign({})
 .then('routes')
 .into(app);
 
+io.on('connection',(client)=>{
+  client.on('send-server',(data)=>{
+    const resposta = `<b>${data.nome}:</b>${data.msg}<br>`;
+    client.emit('send-client',resposta);
+    client.broadcast.emit('send-client',resposta);
+  });
+});
+
 app.use(error.notFound);
 app.use(error.serverError);
 
-app.listen(3000,()=>{
-  console.log('Btalk In the House');
-})
+server.listen(3000, ()=>console.log("Btalk in the house!!"));
